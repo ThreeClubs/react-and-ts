cc6b7e8df6f7364efcbe4f4c46e41b7da52c3687
Your Name <you@example.com>
2023-02-16 18:12:06 +1300

    init commit..

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name: string;
+name = "Andrew"
+
+let age: number;
+age = 37
+
+let isStudent: boolean;
+isStudent = true;
+
+if (!isStudent) {
+  console.log("not a student")
+  }
+else console.log("is a student")
+
+let hobbies: string[];
+hobbies = ["reading", "cooking"]
+
+let numbers: number[];
+numbers = [1,2,3]
+
+let role:[number, string];
+role = [37, "string"]
+
+// type Person= {
+//   name: string;
+//   age?: number;
+// }
+
+// let person: Person = {
+//   name: "Andrew",
+//   age: 37
+// }
+
+// let lotsOfPeople: Person[];
+
+// let stringNumberUnion: number | string;
+
+// stringNumberUnion = 5;
+// stringNumberUnion = "Andrew"
+
+// function printName(name: string) {
+//   console.log(name)
+// }
+
+// printName(name)
+
+let printName: (name:string) => void;
+
+let personName: unknown;  // instead of any type
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Welcome to the world of Typescript and React with {name}, {age}
     </div>
   );
 }

15e907f7e40eab8a5393093ac8ce71cbc8101af7
Andrew Grenon <andrew.grenon@gmail.com>
2023-02-25 14:55:43 +1300

    background filter

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ const App: React.FC = () => {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
-        <span className="heading">Taskify</span>
+        <span className="heading">Woolly-Todo</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
         <TodoList
           todos={todos}
